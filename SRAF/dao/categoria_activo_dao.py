36c5c4117f17a7e61c3511a3ae193e7a5f58a04a
AdrianXZ <joel.solorzanob@istpargentina.edu.pe>
2026-07-29 21:24:55 -0500

    cree actualizar categoria de categoriaactivo dao

--- SRAF/dao/categoria_activo_dao.py
+++ SRAF/dao/categoria_activo_dao.py
@@ -83,3 +83,54 @@ class CategoriaActivioDAO:
         
         return categoria
     
+    def actualizar(self,id_categoria,nombre=None,descripcion=None):
+        conn = obtener_conexion()
+        cursor = conn.cursor()
+        
+        cursor.execute("""
+            SELECT *
+            FROM categorira_activo
+            WHERE id_categoria = ?
+        """, id_categoria)
+        
+        fila = cursor.fetchone()
+        
+        if fila is None:
+            conn.close()
+            
+            raise CategoriaNoEncontradaError(
+                id_categoria
+            )
+            
+        nuevo_nombre = nombre if nombre else fila.nombre
+        
+        nueva_descripcion = (
+            descripcion
+            if descripcion
+            else fila.descripcion
+        )
+        
+        cursor.execute("""
+            UPDATE categoria_activo
+            SET
+                nombre =?,
+                descripcion = ?
+            WHERE id_categoria = ?
+            
+            """,nuevo_nombre,nueva_descripcion,id_categoria
+            )
+        
+        conn.commit()
+        
+        conn.close()
+        
+        categoria = CategoriaActivo(
+            nuevo_nombre,
+            nueva_descripcion
+        )
+        
+        self.logger.info(
+            f"Categoria actualizada ID={id_categoria}"
+        )   
+        
+        return categoria
