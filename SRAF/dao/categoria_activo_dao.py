0ded207afd22a8e3b6947264997f4576d976e314
AdrianXZ <joel.solorzanob@istpargentina.edu.pe>
2026-07-29 20:49:46 -0500

    cree insertar en categoria activodao

--- SRAF/dao/categoria_activo_dao.py
+++ SRAF/dao/categoria_activo_dao.py
@@ -2,7 +2,56 @@ from config.base_datos import obtener_conexion
 from config.logger import logger
 from config.sistema_config import (CategoriaDuplicadaError, CategoriaNoEncontradaError)
 
+from modelos.categoria_activo import CategoriaActivo
+
 class CategoriaActivioDAO:
     def __init__(self):
         self.logger = logger()
         
+    def insertar(self, categoria):
+        
+        conn = obtener_conexion()
+        
+        cursor = conn.cursor()
+        
+        cursor.excecute("""
+            SELECT nombre
+            FROM categoria_activo
+            WHERE nombre = ?
+        """, categoria.nombre)
+        
+        if cursor.fetchone():
+            conn.close()
+            
+            raise CategoriaDuplicadaError(
+                categoria.nombre
+            )
+            
+        cursor.execute("""
+            INSERT INTO categoria_activo
+            (
+                nombre,
+                descripcion
+            )
+            VALUES
+            (
+                ?,?
+            )
+            """, categoria.nombre, categoria.descripcion)
+        
+        conn.commit()
+        
+        cursor.execute("SELECT @@IDENTITY")
+        
+        categoria.id_categoria = cursor.fetchone()[0]
+        
+        conn.close()
+        
+        
+        self.looger.info(
+            f"Categoria registrada ID={categoria.id_categoria}"
+        )
+        
+        return categoria
+    
+    
