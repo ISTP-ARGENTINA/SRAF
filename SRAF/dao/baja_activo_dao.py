2a4a80f7eef484a0136668fc20fc216f79591309
AdrianXZ <joel.solorzanob@istpargentina.edu.pe>
2026-08-02 17:48:53 -0500

    cree insertar en bajaactivodao

---
+++ SRAF/dao/baja_activo_dao.py
@@ -0,0 +1,61 @@
+from config.base_datos import obtener_conexion
+from config.logger import logger
+from config.sistema_config import (BajaNoEncontradaError, BajaDuplicadaError)
+from modelos.baja_activo import BajaActivo
+
+class BajaActivoDAO:
+    def __init__(self):
+        self.logger = logger
+        
+    def insertar(self, baja):
+        conn = obtener_conexion
+        cursor = conn.cursor()
+        
+        cursor.execute("""
+            SELECT id_baja
+            FROM baja_activo
+            WHERE id_activo = ?
+            
+        """,
+        baja.id_activo)
+        
+        if cursor.fetchone():
+            conn.close()
+            
+            raise BajaDuplicadaError(
+                baja.id_activo
+            )
+
+        cursor.excecute("""
+            INSERT INTO baja_activo
+            (
+                motivo,
+                descripcion,
+                id_activo,
+                id_usuario
+            )
+            VALUES
+            (
+                ?,?,?,?
+            )
+        """,
+        baja.motivo,
+        baja.descripcion,
+        baja.id_activo,
+        baja.id_usuario)
+        
+        conn.commit()
+        
+        cursor.excecute("SELECT @@IDENTITY")
+        
+        baja.id_baja = cursor.fetchone()[0]
+        
+        conn.close()
+            
+        self.logger.info(
+            f"baja registrada ID={baja.id_baja}"
+        )
+        
+        return baja
+            
+        
