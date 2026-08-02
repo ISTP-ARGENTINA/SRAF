3a5057cb9f44545d0e305636d760282e808db6fd
AdrianXZ <joel.solorzanob@istpargentina.edu.pe>
2026-08-02 12:58:32 -0500

    cree insertar en detalleinventariodao

---
+++ SRAF/dao/detalle_inventario_dao.py
@@ -0,0 +1,63 @@
+from config.base_datos import obtener_conexion
+from config.logger import logger
+
+from config.sistema_config import (DetalleInventarioDuplicadoError, DetalleInventarioNoEncontradoError)
+from modelos.detalle_inventario import DetalleInventario
+
+class DetalleInventarioDao:
+    def __init__(self):
+        self.logger = logger()
+        
+    def insertar(self, detalle):
+        conn = obtener_conexion()
+        cursor = conn.cursor()
+        
+        cursor.excecute("""
+            SELECT id_detalle
+            FROM detalle_inventario
+            WHERE id_inventario = ?
+            AND id_inventario = ?
+        """,
+        detalle.id_inventario,
+        detalle.id_activo)
+        
+        if cursor.fetchone():
+            conn.close()
+            
+            raise DetalleInventarioDuplicadoError(
+                detalle.id_inventario,
+                detalle.id_activo
+            )
+        
+        cursor.execute("""
+            INSERT INTO detalle_inventario
+            (
+                id_inventario,
+                id_activo,
+                encontrado,
+                observado
+            )
+            VALUES
+            (
+                ?,?,?,?
+            )
+            """,
+            detalle.id_inventario,
+            detalle.id_activo,
+            detalle.encontrado,
+            detalle.observacion)
+        
+        conn.commit()
+        
+        cursor.execute("SELECT @@IDENTITY")
+        
+        detalle.id_detalle = cursor.fetchone()[0]
+        
+        conn.close()
+        
+        self.logger.info(
+            f"Detalle registrado ID={detalle.id_detalle}"
+        )
+        
+        return detalle
+        
